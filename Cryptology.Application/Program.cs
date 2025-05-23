52ffed266dd69359d781077205866268b0d410bc
Sorokovsky <Sorokovskys@ukr.net>
2025-05-23 22:55:04 +0300

    serialization manually chacking

--- Cryptology.Application/Program.cs
+++ Cryptology.Application/Program.cs
@@ -1,19 +1,5 @@
-﻿using System.Text;
-using Cryptology.Encryptions.Rsa;
+﻿using Cryptology.EllipticalCurves;
 
-var encoding = Encoding.UTF8;
-var encryption = new RsaEncryption();
-const int n = 3233;
-var encryptionKey = new RsaEncryptionKey(n, 17);
-var privateKey = new RsaDecryptionKey(n, 2753);
-const string inputString = "Hello, my third world";
-var inputBytes = encoding.GetBytes(inputString);
-var outputBytes = encryption.Encrypt(inputBytes, encryptionKey);
-var outputString = Convert.ToBase64String(outputBytes);
-outputBytes = Convert.FromBase64String(outputString);
-var decryptBytes = encryption.Decrypt(outputBytes, privateKey);
-var decryptedString = encoding.GetString(decryptBytes);
-
-Console.WriteLine($"Input: {inputString}");
-Console.WriteLine($"Encrypted: {outputString}");
-Console.WriteLine($"Decrypted: {decryptedString}");
+var point = new Point(3, 5);
+Console.WriteLine(point.ToString());
+Console.WriteLine(point);
